82f90aa97ee61079d1a85af52bbf3628fb7d6e3e
zhu <zzsheng@gmail.com>
2026-04-03 22:29:49 +0800

    chore(tool): rename fbs to nss_tool and bss_tool

--- infra/fractalbits-bootstrap/src/bss_server.rs
+++ infra/fractalbits-bootstrap/src/bss_server.rs
@@ -69,7 +69,7 @@ pub fn bootstrap(config: &BootstrapConfig, for_bench: bool) -> CmdResult {
     install_packages(&["nvme-cli", "mdadm"])?;
     format_local_nvme_disks(false)?; // no twp support since experiment is done
 
-    let mut binaries = vec!["bss_server", "test_bss_storage_engine", "fbs"];
+    let mut binaries = vec!["bss_server", "test_bss_storage_engine", "nss_tool", "bss_tool"];
     if use_etcd {
         binaries.push("etcdctl");
     }
