b341a8ebc908a3a1933f01d7817af134cfeea140
Thomas <thomas.fractalbits@gmail.com>
2026-09-16 18:16:47 -0700

    refactor(codecs): move shared protos into their rpc codec crates

--- crates/common/rpc_codecs/nss_codec/build.rs
+++ crates/common/rpc_codecs/nss_codec/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    println!("cargo:rerun-if-changed=src/proto/nss_ops.proto");
     prost_build::Config::new()
         .bytes(["."])
         .compile_protos(&["src/proto/nss_ops.proto"], &["src/proto/"])
--- crates/common/rpc_codecs/rss_codec/build.rs
+++ crates/common/rpc_codecs/rss_codec/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    println!("cargo:rerun-if-changed=src/proto/rss_ops.proto");
     prost_build::Config::new()
         .bytes(["."])
         .compile_protos(&["src/proto/rss_ops.proto"], &["src/proto/"])
